3aa530ca41140dd5e02a2a9784335c66351e48e5
unknown <lajiliamal218@gmail.com>
2024-06-03 02:32:27 +0200

    first

--- routes/absence.js
+++ routes/absence.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const absenceController = require("../controllers/absence");
 const auth = require("../controllers/auth");
-
 router.get(
   "/",
   auth.loggedMiddleware,
